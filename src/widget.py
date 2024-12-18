00f810f405c4c2bf969e5b8a275e6e7b8d109702
Arty525 <newart525@yandex.ru>
2024-12-18 22:35:52 +0300

    ready for review

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(input: str) -> str:
     """
     :param input: строка с типом данных и номером карты или счета (например Visa Platinum 7000792289606361)
